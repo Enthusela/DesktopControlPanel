f8f6afc0cb54e8b96f12b51021989bb48984720c
Nathan Weaver <nathanjmsweaver@gmail.com>
2025-11-09 14:01:36 +0800

    Containerised argb_ctrl web app for dev on PC

--- argb_ctrl/web_app/webserver.js
+++ argb_ctrl/web_app/webserver.js
@@ -1,20 +1,40 @@
 var http = require('http').createServer(handler); // create HTTP server with function handler()
 var fs = require('fs');
+var path = require('path');
 
 // Require socket.io and pass the http object
 var io = require('socket.io')(http);
-var Gpio = require('onoff').Gpio;
+
+// GPIO setup with graceful fallback for non-Raspberry Pi systems
+const GPIO_MODE = process.env.GPIO_MODE || 'auto';
+let Gpio, LED, pushButton;
+let gpioAvailable = false;
+
+try {
+    if (GPIO_MODE !== 'mock') {
+        Gpio = require('onoff').Gpio;
+        LED = new Gpio(21, 'out');
+        pushButton = new Gpio(26, 'in', 'both');
+        gpioAvailable = true;
+        console.log('GPIO initialized successfully');
+    } else {
+        console.log('GPIO disabled (mock mode)');
+    }
+} catch (err) {
+    console.warn('GPIO not available:', err.message);
+    console.log('Running in mock GPIO mode - physical button will not work');
+    // Mock GPIO objects
+    LED = { writeSync: () => {}, unexport: () => {} };
+    pushButton = { watch: () => {}, unexport: () => {} };
+}
 
 // Add serialport library to communicate with Arduino
 const { SerialPort } = require('serialport');
+const SERIAL_PORT = process.env.SERIAL_PORT || '/dev/ttyACM0';
 const port = new SerialPort({
-    path: '/dev/ttyACM0',
+    path: SERIAL_PORT,
     baudRate: 9600
 });
-
-// var LED = new Gpio(21, 'out');
-var LED = new Gpio(21, 'out');
-var pushButton = new Gpio(26, 'in', 'both');
 
 // Listen on port 8080
 
@@ -22,12 +42,28 @@ http.listen(8080);
 
 // Create server
 function handler (req, res) {
-    fs.readFile(__dirname + '/public/index.html', function (err, data) {
+    // Serve index.html for root request
+    var filePath = req.url === '/' ? '/public/index.html' : '/public' + req.url;
+    var fullPath = __dirname + filePath;
+
+    // Determine content type
+    var extname = path.extname(fullPath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    fs.readFile(fullPath, function (err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
             return res.end("404 Not Found");
         }
-        res.writeHead(200, {'Content-Type':'text/html'});
+        res.writeHead(200, {'Content-Type': contentType});
         res.write(data);
         return res.end();
     });
@@ -40,13 +76,15 @@ io.sockets.on('connection', function (socket) {
     var saturation = 255;
     var brightness = 255;
     // Get light switch status from server (i.e. button)
-    pushButton.watch(function (err, value) {
-        if (err) {
-            throw err;
-        }
-        lightsOn = value;
-        socket.emit('onOff', lightsOn);
-    });
+    if (gpioAvailable) {
+        pushButton.watch(function (err, value) {
+            if (err) {
+                throw err;
+            }
+            lightsOn = value;
+            socket.emit('onOff', lightsOn);
+        });
+    }
     // Get light switch status from client (i.e. checkbox)
     socket.on('onOff', function(data) {
         lightsOn = data;
